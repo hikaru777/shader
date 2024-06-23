82dc0153634b9fed195b1e198bcae9193eb5735b
HikaruHonda <hikaru.h.developer@gmail.com>
2024-06-23 12:21:30 +0900

    Initial Commit

---
+++ SlopeColorChageEffect/SlopeColorChageEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlopeColorChageEffectApp.swift
+//  SlopeColorChageEffect
+//
+//  Created by 本田輝 on 2024/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SlopeColorChageEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
